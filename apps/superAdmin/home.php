6c20dcd9863f1da702b701a772e9c0e70f5e3bd6
unknown <angeloreyes033001@gmail.com>
2023-04-01 13:05:57 +0800

    first commit

---
+++ apps/superAdmin/home.php
@@ -0,0 +1,73 @@
+<section id="home" >
+                <h1>OVERVIEW</h1>
+                <div class="total-group-box" >
+                    <span>
+                        <h1>0</h1>
+                        <p>Total Professor</p>
+                    </span>
+                    <span>
+                        <h1>0</h1>
+                        <p>Total Professor</p>
+                    </span>
+                    <span>
+                        <h1>0</h1>
+                        <p>Total Professor</p>
+                    </span>
+                </div>
+                <div class="activity-box">
+                    <h1>Recent activities</h1>
+                    <span class="show-all" >See all</span>
+                    <div class="activity-fetch">
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                        <div class="activity-data-box">
+                            <span class="circle" ></span>
+                            <h3>Schedule Update</h3>
+                            <small>March 23, 2023</small>
+                        </div>
+                    </div>
+                </div>
+            </section>
